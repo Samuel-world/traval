b9ef11110e074fd54460403f7bc3cf05b8de3c14
Samuel-world <367197207@qq.com>
2018-06-24 16:19:47 +0800

    框架页

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="home">
+		<h1>hello world</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				msg:'hello world'
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
