c9fbb8f2fec2e076b397fd24edaffe64ceda8c57
Samuel-world <367197207@qq.com>
2018-06-27 23:30:38 +0800

    创建分支

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,19 +1,25 @@
 <template>
 	<div class="home">
+		<Header></Header>
 		<h1>hello world</h1>
 	</div>
 </template>
 
 <script>
+	import Header from './header'
+
 	export default {
 		data(){
 			return {
 				msg:'hello world'
 			}
+		},
+		components:{
+			Header
 		}
 	}
 </script>
 
-<style>
+<style lang="stylus" scoped>
 	
 </style>
